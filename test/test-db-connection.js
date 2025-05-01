5276484f72d69742175834ccaa2e8f8fb0acca7e
Emerson Marques <emersoncmarques@outlook.com>
2025-05-01 18:04:28 -0300

    feat: config docker + mongo

---
+++ test/test-db-connection.js
@@ -0,0 +1,13 @@
+import connectToMongoDB from '../src/config/database.js'
+
+async function testDbConnection() {
+	try {
+		await connectToMongoDB()
+		console.log('Test connection successful')
+	} catch (error) {
+		console.error('Test connection failed:', error.message)
+		process.exit(1)
+	}
+}
+
+testDbConnection()
