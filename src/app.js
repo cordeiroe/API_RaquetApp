bc055ba432d99eb5fe5221e20d281f84f8611eb3
Emerson Marques <emersoncmarques@outlook.com>
2025-05-02 02:02:30 -0300

    refactor: refatoração do projeto

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import AutoLoad from '@fastify/autoload'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const options = {}
+
+export default async function(fastify, opts) {
+	fastify.register(AutoLoad, {
+		dir: path.join(__dirname, 'plugins'),
+		options: Object.assign({}, opts)
+	})
+
+	fastify.register(AutoLoad, {
+		dir: path.join(__dirname, 'routes'),
+		options: Object.assign({}, opts)
+	})
+}
+
+export { options }
