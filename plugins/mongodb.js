c7bf26fc51e40b7a1bf4d7ecccdc31dd1c5683a3
Emerson Marques <emersoncmarques@outlook.com>
2025-05-05 17:45:10 -0300

    fix: remocao arquivos duplicados

--- plugins/mongodb.js
+++
@@ -1,8 +0,0 @@
-import fp from 'fastify-plugin'
-import mongoose from 'mongoose'
-import connectToMongoDB from '../src/config/database'
-
-export default fp(async (fastify) => {
-	await connectToMongoDB()
-	fastify.decorate('mongo', mongoose)
-})
