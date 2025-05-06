41418b9922ab0c25f63f3aae60af2ab783333851
Emerson Marques <emersoncmarques@outlook.com>
2025-05-06 11:54:39 -0300

    feat: criação das rotas a partir dos schemas de professores

--- src/routes/teachers/index.js
+++ src/routes/teachers/index.js
@@ -1,13 +1,39 @@
-import { createTeacherProfile } from './teachers.controller.js'
+import {
+	createTeacherProfile,
+	getTeacherProfile,
+	updateTeacherProfile,
+	deleteTeacherProfile,
+	searchTeacherProfile
+} from './teachers.controller.js'
+import teacherSchema from './teachers.schema.js'
 
 export default async function (fastify, opts) {
 	fastify.post('/', {
 		onRequest: [fastify.authenticate],
+		schema: teacherSchema.createTeacherProfile,
 		handler: createTeacherProfile
 	})
 
-	// To-do:
-	// - Rota para buscar perfil de professor
-	// - Rota para atualizar perfil de professor
-	// - Rota para deletar perfil de professor
+	fastify.get('/', {
+		onRequest: [fastify.authenticate],
+		schema: teacherSchema.getTeacherProfile,
+		handler: getTeacherProfile
+	})
+
+	fastify.put('/', {
+		onRequest: [fastify.authenticate],
+		schema: teacherSchema.updateTeacherProfile,
+		handler: updateTeacherProfile
+	})
+
+	fastify.delete('/', {
+		onRequest: [fastify.authenticate],
+		schema: teacherSchema.deleteTeacherProfile,
+		handler: deleteTeacherProfile
+	})
+
+	fastify.get('/search', {
+		schema: teacherSchema.searchTeacherProfile,
+		handler: searchTeacherProfile
+	})
 }
