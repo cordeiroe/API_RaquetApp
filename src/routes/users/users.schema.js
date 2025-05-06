94629ff87cf78645b4cef5d4927eb17d48e00872
Emerson Marques <emersoncmarques@outlook.com>
2025-05-06 00:39:40 -0300

    refactor: utilização do helper criado

--- src/routes/users/users.schema.js
+++ src/routes/users/users.schema.js
@@ -1,11 +1,4 @@
-const errorResponse = {
-	type: 'object',
-	properties: {
-		statusCode: { type: 'number', description: 'Código de status' },
-		message: { type: 'string', description: 'Mensagem de erro' },
-		error: { type: 'string', description: 'Descrição do erro' }
-	}
-}
+import { errorResponse } from '../../schemas/common.js'
 
 const userSchema = {
 	createUser: {
